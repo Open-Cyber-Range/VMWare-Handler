9e6f3aadb22386cc237d0493bfac6b01519233aa
Rait Rotšan <rait.rotsan@cr14.ee>
2023-07-24 15:54:16 +0300

    remove dead code

--- library/vmware.go
+++ library/vmware.go
@@ -356,28 +356,6 @@ func normalizePackageTargetPath(sourcePath string, destinationPath string) strin
 	return destinationPath
 }
 
-func sendFileToVM(url string, filePath string) (err error) {
-	transport := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
-	client := http.Client{Transport: transport, Timeout: 30 * time.Second}
-	file, err := os.ReadFile(filePath)
-	if err != nil {
-		return status.Error(codes.Internal, fmt.Sprintf("Error reading source file, %v", err))
-	}
-
-	request, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(file))
-	if err != nil {
-		return status.Error(codes.Internal, fmt.Sprintf("Error creating HTTP request, %v", err))
-	}
-
-	response, err := client.Do(request)
-	if err != nil {
-		return status.Error(codes.Internal, fmt.Sprintf("Error sending HTTP request: %v", err))
-	}
-	if response.StatusCode != 200 {
-		return status.Error(codes.Internal, fmt.Sprintf("Error while uploading file: %v", response.Status))
-	}
-	return err
-}
 
 func receiveFileFromVM(url string) (logPath string, err error) {
 	out, err := os.CreateTemp("", "executor.log")
